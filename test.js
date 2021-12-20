0b338c29ba4db77ed2bb51b291677e3b8f72e088
TheGeoffMeister <geoff.barrett@protonmail.com>
2021-12-19 20:06:14 -0500

    init

---
+++ test.js
@@ -0,0 +1,14 @@
+let apple = 'red';
+let pear = 1.0;
+let orange = 50;
+let boolVar = true;
+
+const cherry = 10;
+
+
+
+console.log(apple, pear, orange, boolVar, cherry);
+
+let myObject = {
+    apple = 
+};
