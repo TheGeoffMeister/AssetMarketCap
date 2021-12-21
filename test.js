7a0031e366c7d44168bf839a311be0cb2e0cbb3b
TheGeoffMeister <geoff.barrett@protonmail.com>
2021-12-20 19:12:54 -0500

    init

--- test.js
+++ test.js
@@ -1,14 +1,39 @@
+//variables
 let apple = 'red';
 let pear = 1.0;
 let orange = 50;
 let boolVar = true;
 
+
+//constants
 const cherry = 10;
 
-
-
+// print to screen
 console.log(apple, pear, orange, boolVar, cherry);
 
+// create an object / dictionary
 let myObject = {
-    apple = 
+    name: 'green',
+    age: '12'
+
 };
+
+console.log(myObject.name, myObject['age']);
+
+// arrays
+let selectedColors = ['red', 'green', 'blue'];
+console.log(selectedColors[0])
+console.log(selectedColors.length)
+
+// functions
+function greet() {
+    console.log('hello world')
+};
+
+greet()
+
+function greetings(name) {
+    console.log('Hello '+'John')
+};
+
+greetings('John')
